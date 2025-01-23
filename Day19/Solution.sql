ae970d5b626a067da08ecdc7694a58980f2a13fb
lakkoju.sandeep <lakkoju.sandeep@werize.com>
2025-01-23 09:04:34 +0530

    Day19

---
+++ Day19/Solution.sql
@@ -0,0 +1,17 @@
+
+/*
+Self join the tables 3 times while the topping name in the
+left table is less than that of the right table.
+Concatenate the names and sum the costs
+*/
+SELECT
+  pt1.topping_name || ',' || pt2.topping_name || ',' || pt3.topping_name AS pizza,
+  pt1.ingredient_cost + pt2.ingredient_cost + pt3.ingredient_cost AS total_cost
+FROM pizza_toppings AS pt1
+JOIN pizza_toppings AS pt2
+  ON pt1.topping_name < pt2.topping_name
+JOIN pizza_toppings AS pt3
+  ON pt2.topping_name < pt3.topping_name
+ORDER BY total_cost DESC, pizza
+;
+
